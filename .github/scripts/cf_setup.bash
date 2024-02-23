066920672c6f504aa7483bfc4153fe3a30e39ac1
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-02-23 16:58:59 +0100

    added vars

--- .github/scripts/cf_setup.bash
+++ .github/scripts/cf_setup.bash
@@ -16,20 +16,19 @@
 # Iterate over the keys and extract values
 while read -r key; do
     # For the Provider authentication
-    cf_api_endpoint=$(echo "$json_string" | jq -r ".$key.cf_api_endpoint")
+    cf_api_endpoint=$(echo "$CF_API_DETAILS" | jq -r ".$key.cf_api_endpoint")
     # For the variables
-    cf_org_id=$(echo "$json_string" | jq -r ".$key.cf_org_id")
-
+    cf_org_id=$(echo "$CF_API_DETAILS" | jq -r ".$key.cf_org_id")
+    admins=$(echo "$CF_API_DETAILS" | jq -r ".$key.admins")
+    cf_org_name=$(echo $CF_API_DETAILS | jq -r ".$key.cf_org_name")
+   
     # Set environment variables
     export "$CF_API_URL=$cf_api_endpoint"
     
     terraform -chdir=$PATH_TO_TFSCRIPT_CF init -no-color 
-    terraform -chdir=$PATH_TO_TFSCRIPT_CF apply -auto-approve -no-color -var cf_org_id=$cf_org_id
+    terraform -chdir=$PATH_TO_TFSCRIPT_CF apply -auto-approve -no-color -var cf_org_id=$cf_org_id -var admins=$admins -var cf_org_name=$cf_org_name -var folder_cap_mtar='src/.mta_archives/xyz.tbd'
 
-    echo "Set ${key}_CF_API_ENDPOINT=$cf_api_endpoint"
-    echo "Set ${key}_CF_ORG_ID=$cf_org_id"
+    #echo "Set ${key}_CF_API_ENDPOINT=$cf_api_endpoint"
+    #echo "Set ${key}_CF_ORG_ID=$cf_org_id"
 done < <( echo "$CF_API_DETAILS" | jq -r 'keys[]' )
 #done < <( echo "$json_string" | jq -r 'keys[]' )
-
-
-
