3300e88f3245527e84fed8c2c4b8d1bdcd443893
Rui Nogueira <rui.nogueira@sap.com>
2024-02-26 16:53:48 +0000

    add hana to tf setup

--- infra/setup_cf_spaces/main.tf
+++ infra/setup_cf_spaces/main.tf
@@ -9,3 +9,15 @@ module "cloudfoundry_space" {
   cf_space_developers = ["rui.nogueira@sap.com", "christian.lechner@sap.com"]
   cf_space_auditors   = ["rui.nogueira@sap.com", "christian.lechner@sap.com"]
 }
+
+
+data "cloudfoundry_service" "hana_cloud" {
+  name = "hana-cloud"
+}
+
+resource "cloudfoundry_service_instance" "hana_cloud_hana" {
+  name         = "hana-cloud"
+  space        = module.cloudfoundry_space.space.id
+  service_plan = data.cloudfoundry_service.hana_cloud.service_plans["hana"]
+  json_params  = jsonencode({"data": {"memory": 30,"edition": "cloud","systempassword": "Abcd1234", "whitelistIPs": ["0.0.0.0/0"]}})
+}
