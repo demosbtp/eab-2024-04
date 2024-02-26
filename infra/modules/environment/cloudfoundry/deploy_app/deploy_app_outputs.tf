af6fc9f50387a3122ecf76660ea8e3488140d54e
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-02-26 08:54:42 +0100

    Redesign of Workflow - Modularization (#8)

--- infra/modules/environment/cloudfoundry/deploy_app/deploy_app_outputs.tf
+++
@@ -1,4 +0,0 @@
-output "app_info" {
-  value       = null_resource.cf_app_deploy
-  description = "The metadata of the deployed app."
-}
--- infra/setup_cf_spaces/variables.tf
+++ infra/setup_cf_spaces/variables.tf
@@ -1,24 +1,14 @@
-variable "admins" {
-  type        = list(string)
-  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
-}
-
 variable "cf_org_id" {
   type        = string
   description = "ID of the Cloud Foundry org."
 }
 
-variable "cf_api_endpoint" {
+variable "cf_space_name" {
   type        = string
-  description = "API endpoint of the Cloud Foundry environment."
+  description = "Name of the Cloud Foundry space."
 }
 
-variable "cf_org_name" {
-  type        = string
-  description = "Name of the Cloud Foundry org."
-}
-
-variable "folder_cap_mtar" {
-  type        = string
-  description = "Path to the mta archive file."
+variable "admins" {
+  type        = list(string)
+  description = "Defines the colleagues who are added to each subaccount as emergency administrators."
 }
