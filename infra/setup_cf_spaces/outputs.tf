f25642247964b6ed0aad41f47e6cdb549351fbb3
Rui Nogueira <rui.nogueira@sap.com>
2024-02-27 04:59:05 +0000

    correct name for space id

--- infra/setup_cf_spaces/outputs.tf
+++ infra/setup_cf_spaces/outputs.tf
@@ -1,4 +1,4 @@
 output "cf_space_id" {
-  value       = module.cloudfoundry_space.cf_space_id
+  value       = module.cloudfoundry_space.space_id
   description = "ID of the Cloud Foundry space."
 }
