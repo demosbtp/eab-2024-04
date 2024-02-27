671c767ad9001637d4e4ec56f665fb7dabea744d
Rui Nogueira <rui1610@users.noreply.github.com>
2024-02-27 06:23:19 +0100

    All cf in terraform (#14)
    * add hana on cf
    * correct name for space id
    * fix issue in env setup

---
+++ infra/setup_cf_spaces/outputs.tf
@@ -0,0 +1,4 @@
+output "cf_space_id" {
+  value       = module.cloudfoundry_space.space_id
+  description = "ID of the Cloud Foundry space."
+}
---
+++ infra/setup_hana_on_cf/hana_variables.tf
@@ -0,0 +1,5 @@
+variable "cf_space_id" {
+  type        = string
+  description = "ID of the Cloud Foundry space."
+}
+
