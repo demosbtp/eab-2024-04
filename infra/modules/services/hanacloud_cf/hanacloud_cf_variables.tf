6fba470fd21da9bcba888ed2a8a97c57392becc4
Christian Lechner <lechnerc77@users.noreply.github.com>
2024-02-27 08:15:08 +0100

    restruc hana instance

--- infra/modules/services/hanacloud_cf/hanacloud_cf_variables.tf
+++ infra/modules/services/hanacloud_cf/hanacloud_cf_variables.tf
@@ -2,4 +2,3 @@ variable "cf_space_id" {
   type        = string
   description = "ID of the Cloud Foundry space."
 }
-
--- infra/setup_cf_spaces/main.tf
+++ infra/setup_cf_spaces/main.tf
@@ -5,7 +5,24 @@ module "cloudfoundry_space" {
   source              = "../modules/environment/cloudfoundry/space_cf"
   cf_org_id           = var.cf_org_id
   name                = var.cf_space_name
-  cf_space_managers   = ["rui.nogueira@sap.com", "christian.lechner@sap.com"]
-  cf_space_developers = ["rui.nogueira@sap.com", "christian.lechner@sap.com"]
-  cf_space_auditors   = ["rui.nogueira@sap.com", "christian.lechner@sap.com"]
+  cf_space_managers   = ["rui.nogueira@sap.com"]
+  cf_space_developers = ["rui.nogueira@sap.com"]
+  cf_space_auditors   = ["rui.nogueira@sap.com"]
+}
+
+
+# ------------------------------------------------------------------------------------------------------
+# Timeout - Ensure that the service entitlement is propagated to CF Marketplace
+# ------------------------------------------------------------------------------------------------------
+resource "time_sleep" "wait_for_cfmarketplace" {
+  create_duration = "30s"
+}
+
+# ------------------------------------------------------------------------------------------------------
+# Create Service Instance for HANA Cloud
+# ------------------------------------------------------------------------------------------------------
+module "cloudfoundry_hana_cloud" {
+  source      = "../modules/services/hanacloud_cf"
+  cf_space_id = module.cloudfoundry_space.cf_space_id
+  depends_on  = [time_sleep.wait_for_cfmarketplace]
 }
