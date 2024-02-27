43b1824e1353b630b7f749632473875b96eba0af
Rui Nogueira <rui.nogueira@sap.com>
2024-02-27 04:53:31 +0000

    add hana on cf

---
+++ infra/setup_cf_spaces/outputs.tf
@@ -0,0 +1,4 @@
+output "cf_space_id" {
+  value       = module.cloudfoundry_space.cf_space_id
+  description = "ID of the Cloud Foundry space."
+}
---
+++ infra/setup_hana_on_cf/hana_provider.tf
@@ -0,0 +1,12 @@
+
+terraform {
+  required_providers {
+    cloudfoundry = {
+      source  = "cloudfoundry-community/cloudfoundry"
+      version = "~>0.53.0"
+    }
+  }
+}
+
+// Configuration is described in https://registry.terraform.io/providers/cloudfoundry-community/cloudfoundry/latest/docs
+provider "cloudfoundry" {}
