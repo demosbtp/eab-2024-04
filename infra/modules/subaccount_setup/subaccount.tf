541f7fd5f56586725731c8c2bd1080cc8a5218e2
Rui Nogueira <rui1610@users.noreply.github.com>
2024-03-14 22:19:03 +0100

    Update subaccount.tf (#96)

--- infra/modules/subaccount_setup/subaccount.tf
+++ infra/modules/subaccount_setup/subaccount.tf
@@ -11,7 +11,7 @@ terraform {
 # Setup subaccount domain (to ensure uniqueness in BTP global account)
 # ------------------------------------------------------------------------------------------------------
 locals {
-  project_subaccount_domain = "btpeab24"
+  project_subaccount_domain = "btpeab2404"
 
   role_mapping_admins = distinct(flatten([
     for admin in var.admins : [
